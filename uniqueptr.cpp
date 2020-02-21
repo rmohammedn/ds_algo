d0f96b94601bd645baab4014e212c3a45eeb26d8
rmohammed <sharathk@kpit.com>
2020-02-21 17:56:09 +0530

    unique_ptr

--- uniqueptr.cpp
+++ uniqueptr.cpp
@@ -5,6 +5,7 @@ class Resource
 {
 public:
     Resource();
+    friend std::ostream& operator<<(std::ostream &out, Resource &res);
     ~Resource();
 };
 
@@ -17,10 +18,24 @@ Resource::~Resource()
 {
     std::cout << "Resource object deallocated" << std::endl;
 }
+std::ostream& operator<<(std::ostream &out, Resource &res)
+{
+    out << "I am in print" << std::endl;
+    return out;
+}
+
+
+void useResource(std::unique_ptr<Resource> &ptr)
+{
+    std::cout << *ptr << std::endl;
+}
+
 
 int main()
 {
-    std::unique_ptr<Resource> res1{new Resource};
-    std::unique_ptr<Resource> res2 = res1;
+    std::unique_ptr<Resource> res = std::make_unique<Resource>();
+    std::shared_ptr<Resource> res2 = std::make_shared<Resource>();
+    auto res3 = res2;
+    useResource(res);
     return 0;
 }
